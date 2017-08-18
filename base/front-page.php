e8133ec09140842196d4fe238c62aaafede96f74
Jinma Yamashita <jinmayamashita@users.noreply.github.com>
2017-08-18 21:01:20 +0900

    Create front-page.php

---
+++ base/front-page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<main>
+  <h1>front page</h1>
+  <ul>
+    <li>
+      <a href="/about">
+        about
+      </a>
+    </li>
+    <li>
+      <a href="/contact">
+        contact
+      </a>
+    </li>
+  </ul>
+</main>
+<?php get_footer(); ?>
